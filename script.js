faf539eecc1468bec90d28255b202cf5401454e2
olyasaint <olya@ostryk.com>
2014-11-18 14:31:01 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var object1 = {
+    name: "Olya",
+    age: 35
+};
+
+var object2 = {
+    weather: "Hot",
+    temperature: 35
+};
+
+var object3 = {
+    mood: "Happy",
+    activity: "Dancing"
+};
