3e74e892b35a4358493cb9e4e44412dc2216ec9d
olyasaint <olya@ostryk.com>
2014-11-20 13:02:23 -0800

    Create LiteralNotationVsConstructorNotation.script.js

---
+++ LiteralNotationVsConstructorNotation.script.js
@@ -0,0 +1,9 @@
+// Here is susan1, in literal notation
+var susan1 = {
+  name: "Susan Jordan",
+  age: 24
+};
+// Here is susan2 using a constructor instead
+var susan2 = new Object ();
+susan2.name = "Susan Jordan";
+susan2.age = 24;
