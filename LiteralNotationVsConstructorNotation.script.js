47250bbb9c1375be83ba18c15df0d5728ff1e39f
olyasaint <olya@ostryk.com>
2014-11-20 13:08:58 -0800

    Update LiteralNotationVsConstructorNotation.script.js

--- LiteralNotationVsConstructorNotation.script.js
+++ LiteralNotationVsConstructorNotation.script.js
@@ -7,3 +7,16 @@ var susan1 = {
 var susan2 = new Object ();
 susan2.name = "Susan Jordan";
 susan2.age = 24;
+
+// help us make snoopy using literal notation
+// Remember snoopy is a "beagle" and is 10 years old.
+var snoopy = {
+    species: "beagle",
+    age: 10
+}
+
+// help make buddy using constructor notation
+// buddy is a "golden retriever" and is 5 years old
+var buddy = new Object ()
+buddy.species = "golden retriever";
+buddy.age = 5;
