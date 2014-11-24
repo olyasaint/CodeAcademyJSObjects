4594374e8bd9bed7153c972809920f2367bd5f6f
olyasaint <olya@ostryk.com>
2014-11-24 13:35:43 -0800

    Update AddressBook.script.js

--- AddressBook.script.js
+++ AddressBook.script.js
@@ -25,3 +25,25 @@ var mary = {
 var contacts = [bob, mary];
 console.log (contacts[1].phoneNumber)
 //output:(650) 888-8888
+
+//print out information about any person in our contacts
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+var contacts = [bob, mary];
+// printPerson added here
+var printPerson = function (person) {
+    console.log (person.firstName + " " + person.lastName)
+}
+printPerson(contacts[0]);
+printPerson(contacts[1]);
+//output:Bob Jones Mary Johnson
