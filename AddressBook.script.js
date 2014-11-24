ac637b658f5b207aedae460cf7ff5f189471d1b2
olyasaint <olya@ostryk.com>
2014-11-24 13:10:44 -0800

    Create AddressBook.script.js

---
+++ AddressBook.script.js
@@ -0,0 +1,10 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-777",
+    email: "bob.jones@example.com"
+};
+// print values of first name, last name and email
+console.log(bob.firstName);
+console.log(bob.lastName);
+console.log(bob.email);
